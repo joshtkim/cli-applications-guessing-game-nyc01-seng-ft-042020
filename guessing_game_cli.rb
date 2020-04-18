48a4a1dbe896769a5e201456f88e0ebe6021157c
Joshua Kim <jykim92@gmail.com>
2020-04-18 22:35:18 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,7 +6,7 @@ def run_guessing_game
 
 if user_input == number.to_s
   return "You guessed the correct number!"
-elsif user_input != number.to_s
+else user_input != number.to_s
   return "Sorry! The computer guessed #{number}"
 elsif user_input == "exit"
   puts "Goodbye!"
